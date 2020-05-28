bc90ca0e52cfb2922c3767536d3d12f062216910
Stein47 <63746775+Stein47@users.noreply.github.com>
2020-05-27 21:21:25 -0700

    Delete MyException.java

--- MyException.java
+++
@@ -1,8 +0,0 @@
-package com.cis143.m05.m05_student_code.student.lab.trycatch;
-
-public class MyException extends Exception{
-	public MyException(String message, Throwable t) {
-		super(message, t);
-	}
-
-}
